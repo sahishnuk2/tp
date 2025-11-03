77390a08c75f56fab8407208274174e87be0ecd2
Sahishnu <sahishnuk2@gmail.com>
2025-11-03 18:34:59 +0800

    Fix checkstyle main errors

--- src/main/java/seedu/address/logic/parser/SetWeekCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SetWeekCommandParser.java
@@ -2,12 +2,6 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_GITHUB_USERNAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENTID;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.commands.SetWeekCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
